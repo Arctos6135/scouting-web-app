09282d76be1abb02a4f9d3e1f3a52c3aeeb7a56f
Alex Waese-Perlman <alexwaeseperlman@gmail.com>
2022-03-05 01:38:20 -0500

    Add form creation and deletion

--- backend/admin.ts
+++ backend/admin.ts
@@ -2,6 +2,9 @@ import * as socketio from 'socket.io';
 import models from './db';
 import {RegisterResult} from './db/models/User';
 import {LoginResult, Scout} from './db/models/Scouting';
+import { Form } from './db/models/Form';
+import FormClass, { Section } from '../formSchema/Form';
+
 export default async function addListeners(socket: socketio.Socket, io: socketio.Server) {
 	const req: any = socket.handshake;
 
@@ -13,8 +16,10 @@ export default async function addListeners(socket: socketio.Socket, io: socketio
 		}
 	});
 
+	const admin = () => !!req.session.scout?.admin;
+
 	const sendScouts = async () => {
-		if (!req.session.scout?.admin) {
+		if (!admin()) {
 			socket.emit('organization:get scouts', []);
 			return;
 		}
@@ -23,10 +28,18 @@ export default async function addListeners(socket: socketio.Socket, io: socketio
 		socket.emit('organization:get scouts', scouts);
 	}
 
-	const admin = () => !!req.session.scout?.admin;
+	const sendForms = async () => {
+		if (!admin()) {
+			socket.emit('organization:get forms', []);
+			return;
+		}
 
+		const forms = await Form.find({ ownerOrg: req.session.scout.org });
+		socket.emit('organization:get forms', forms);
+	}
 
 	socket.on('organization:get scouts', sendScouts);
+	socket.on('organization:get forms', sendForms);
 
 	socket.on('organization:update password', async ({login, newPassword}: {login: string; newPassword: string;}) => {
 		// Don't allow operation for non-admins
@@ -51,12 +64,32 @@ export default async function addListeners(socket: socketio.Socket, io: socketio
 		const scout = await Scout.findOne({org: req.session.scout.org, login}).exec();
 		if (scout && !scout.admin) if (await scout.delete()) return socket.emit('organization:delete scout', true); 
 		socket.emit('organization:delete scout', false);
-	})
+	});
+	
+	socket.on('organization:update form', async (form: {name: string; sections: Section[]}) => {
+		if (!admin()) return socket.emit('organization:update form', false);
+		const newForm = new Form({...form, ownerOrg: req.session.scout.org});
+		newForm.save();
+	});
 
+	socket.on('organization:delete form', async (form: {name: string}) => {
+		if (!admin()) return socket.emit('organization:update form', false);
+		const current = await Form.findOne({ org: req.session.scout.org,name: form.name }).exec();
+		if (current) await current.delete();
+	});
+
+	// TODO: There should only be one set of these listeners in the server
+	// New connections can subscribe to relevant listeners
 	const scoutEvents = Scout.watch();
 	scoutEvents.on('change', async (change: any) => {
 		if (!req.session.scout?.admin) return;
 		sendScouts();
+	});
+
+	const formEvents = Form.watch();
+	formEvents.on('change', async () => {
+		if (!req.session.scout?.admin) return;
+		sendForms();
 	});
 
 	socket.on('disconnect', () => {
--- src/Admin/index.tsx
+++ src/Admin/index.tsx
@@ -3,6 +3,7 @@ import { Button, CloseButton, Container, Form, InputGroup, Table } from "react-b
 import { useNavigate } from 'react-router';
 import { useRecoilValue } from 'recoil';
 import { ScoutClass } from '../../backend/db/models/Scouting';
+import FormClass from '../../formSchema/Form';
 import * as conn from '../connection';
 import { DeleteModal } from './DeleteModal';
 import { ErrorModal } from './ErrorModal';
@@ -10,23 +11,40 @@ import { RegisterModal } from './RegisterModal';
 import { UpdatePasswordModal } from './UpdatePasswordModal';
 const {useState, useEffect} = React;
 
+function createForm(forms: FormClass[]) {
+	let nameSuffix = 0;
+	while (forms.some((form) => form.name == `Form #${nameSuffix}`)) nameSuffix++;
+	conn.socket.emit('organization:update form', { name: `Form #${nameSuffix}`, sections: [] })
+}
+
 export default function AdminPage() {
 	const [loginLink, setLoginLink] = useState<string>("Loading...");
 	const signedIn = useRecoilValue(conn.signedIn);
 
 	const [scouts, setScouts] = useState<ScoutClass[]>([]);
 	const selfScout = useRecoilValue(conn.scout);
-	console.log(scouts);
+
+	const [forms, setForms] = useState<FormClass[]>([]);
+
 	const navigate = useNavigate();
+
 	
 	useEffect(() => {
 		if (!signedIn) {
 			navigate("/home", {replace: true});
 		}
-	});
+		else {
+			conn.socket.emit('organization:get scouts');
+			conn.socket.emit('organization:get forms');
+		}
+	}, [signedIn]);
 
 	conn.useSocketEffect('organization:get scouts', (scouts: ScoutClass[]) => {
 		setScouts(scouts);
+	});
+
+	conn.useSocketEffect('organization:get forms', (forms: FormClass[]) => {
+		setForms(forms);
 	});
 
 	useEffect(() => {
@@ -37,6 +55,7 @@ export default function AdminPage() {
 	});
 
 	const [deletingScout, setDeletingScout] = useState<ScoutClass>(null);
+	const [deletingForm, setDeletingForm] = useState<FormClass>(null);
 
 	const setAdmin = (scout: ScoutClass, admin: boolean) => {
 
@@ -44,8 +63,8 @@ export default function AdminPage() {
 
 	const [updatingPassword, setUpdatingPassword] = useState<ScoutClass>(null);
 	const [errorPopup, setErrorPopup] = useState<string>(null);
-
 	const [creatingScout, setCreatingScout] = useState(false);
+	const [editingForm, setEditingForm] = useState<FormClass>(null);
 
 	conn.useSocketEffect('organization:update password', (result: boolean) => {
 		if (!result) setErrorPopup('Password update failed');
@@ -59,10 +78,56 @@ export default function AdminPage() {
 		if (!result) setErrorPopup('Failed to create scout');
 	}, [signedIn]);
 
+	conn.useSocketEffect('organization:update form', (result: boolean) => {
+		if (!result) setErrorPopup('Failed to update form');
+	}, [signedIn]);
+
+	const ScoutTable = <Table>
+		<thead>
+			<tr>
+				<th>Name</th>
+				<th>Login</th>
+				<th>Connections</th>
+				<th>Admin</th>
+			</tr>
+		</thead>
+		<tbody>
+			{scouts.map(scout => <tr className={scout.login == selfScout.login ? 'table-primary' : ''} key={scout.name}>
+				<th>{scout.name}</th>
+				<th>{scout.login}</th>
+				<th>{scout.connections}</th>
+				<th><Form.Check onClick={() => setAdmin(scout, !scout.admin)} type='switch' defaultChecked={scout.admin} disabled={scout.login == selfScout.login} /></th>
+				<th><Button onClick={() => setUpdatingPassword(scout)}>Update password</Button></th>
+				<th><CloseButton onClick={() => setDeletingScout(scout)} disabled={scout.login == selfScout.login} /></th>
+			</tr>)}
+		</tbody>
+	</Table>;
+
+	console.log(forms);
+
+	const FormTable = <Table>
+		<thead>
+			<tr>
+				<th>Name</th>
+			</tr>
+		</thead>
+		<tbody>
+			{forms.map(form => <tr key={form.name}>
+				<th>{form.name}</th>
+				<th><Button onClick={() => setEditingForm(form)}>Edit</Button></th>
+				<th><CloseButton onClick={() => setDeletingForm(form)} /></th>
+			</tr>)}
+		</tbody>
+	</Table>;
 	return <>
-		<DeleteModal bodyText={`All information related to this scout will be deleted (including assigned projects). Username: ${deletingScout?.name}`} show={!!deletingScout} onClose={(del) => {
+		<DeleteModal bodyText={`All information related to this scout will be deleted. Username: ${deletingScout?.name}`} show={!!deletingScout} onClose={(del) => {
 			if (del) conn.socket.emit('organization:delete scout', deletingScout.login);
 			setDeletingScout(null);
+		}}/>
+
+		<DeleteModal bodyText={`All information related to this form will be deleted. Name: ${deletingForm?.name}`} show={!!deletingForm} onClose={(del) => {
+			if (del) conn.socket.emit('organization:delete form', {name: deletingForm.name});
+			setDeletingForm(null);
 		}}/>
 
 		<UpdatePasswordModal show={!!updatingPassword} onClose={(result) => {
@@ -86,30 +151,15 @@ export default function AdminPage() {
 				<Button onClick={() => navigator.clipboard.writeText(loginLink)}>Copy</Button>
 			</InputGroup>
 			<h2>Scouts</h2>
-			<Table>
-				<thead>
-					<tr>
-						<th>Name</th>
-						<th>Login</th>
-						<th>Connections</th>
-						<th>Admin</th>
-						<th></th>
-						<th></th>
-					</tr>
-				</thead>
-				<tbody>
-					{scouts.map(scout => <tr className={scout.login == selfScout.login ? 'table-primary' : ''} key={scout.name}>
-						<th>{scout.name}</th>
-						<th>{scout.login}</th>
-						<th>{scout.connections}</th>
-						<th><Form.Check onClick={() => setAdmin(scout, !scout.admin)} type='switch' defaultChecked={scout.admin} disabled={scout.login==selfScout.login}/></th>
-						<th><Button onClick={() => setUpdatingPassword(scout)}>Update password</Button></th>
-						<th><CloseButton onClick={() => setDeletingScout(scout)} disabled={scout.login==selfScout.login} /></th>
-					</tr>)}
-				</tbody>
-			</Table>
+			{ScoutTable}
 			<Button onClick={() => setCreatingScout(true)}>
 				Add scout
+			</Button>
+
+			<h2>Forms</h2>
+			{FormTable}
+			<Button onClick={() => createForm(forms)}>
+				Add form
 			</Button>
 		</Container>
 	</>
