07923a3123587b8e5f7f279c817c57bb35c8e5aa
Gabriel Dinner-David <gabydinnerdavid@gmail.com>
2022-03-16 02:01:36 -0400

    add pwa functionality

---
+++ src/sw.ts
@@ -0,0 +1,20 @@
+import { registerRoute, NavigationRoute } from 'workbox-routing';
+import { StaleWhileRevalidate } from 'workbox-strategies';
+import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
+
+declare const self: ServiceWorkerGlobalScope;
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+const handler = createHandlerBoundToURL('/index.html');
+const navigationRoute = new NavigationRoute(handler);
+registerRoute(navigationRoute);
+
+registerRoute(
+	({ request }) => request.destination === 'script' ||
+		request.destination === 'style' ||
+		request.destination === 'image',
+	new StaleWhileRevalidate({
+		cacheName: 'static-resources',
+	})
+);
