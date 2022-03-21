ff1348bf51d43230339272ac107d3ca36ca3d79e
Alex Waese-Perlman <alexwaeseperlman@gmail.com>
2022-03-21 14:16:56 -0400

    add serialization

--- shared/dataClasses/FormClass/Number.ts
+++ shared/dataClasses/FormClass/Number.ts
@@ -1,13 +1,38 @@
-import { prop } from '@typegoose/typegoose';
+import {prop} from '@typegoose/typegoose';
+import assert from 'assert';
 import FormComponent from './FormComponent';
 
 class Num extends FormComponent {
 	@prop()
 	readonly type = 'num';
+	
 	@prop() min: number;
 	@prop() max: number;
+	@prop() increment: number;
 
 	@prop() default?: string;
+
+	static serialize(data: number, previous: bigint, num: Num): bigint {
+		const min = num.min/num.increment;
+		const max = num.max/num.increment;
+		const intVal = Math.round((data-min)/num.increment);
+		assert(0 <= data);
+		assert(data <= max-min);
+		console.log(intVal, max, min);
+		return previous * BigInt(max-min) + BigInt(intVal)
+	}
+
+	static deserialize(data: bigint, num: Num): { data: number, remaining: bigint } {
+		const min = num.min/num.increment;
+		const max = num.max/num.increment;
+		const intVal = data%BigInt(max-min);
+		const val = (Number(intVal)+min)*num.increment;
+		console.log(intVal, max, min);
+		return {
+			data: val,
+			remaining: data/BigInt(max-min)
+		}
+	}
 }
 
 export default Num;
--- shared/dataClasses/FormClass/index.ts
+++ shared/dataClasses/FormClass/index.ts
@@ -1,4 +1,5 @@
-import { prop } from '@typegoose/typegoose';
+import {prop} from '@typegoose/typegoose';
+import assert from 'assert';
 import Num from './Number';
 import Picker from './Picker';
 import Text from './Text';
@@ -23,6 +24,68 @@ export class Section {
 	readonly type: string = 'section';
 	@prop() header?: string;
 	@prop() groups: (Group | Row)[];
+}
+
+const constructorMap: {
+	'num': typeof Num;
+	'picker': typeof Picker;
+	'text': typeof Text;
+} = {
+	'num': Num,
+	'picker': Picker,
+	'text': Text
+}
+
+function extractGroups(schema: Section[]): Group[] {
+	const groups = [];
+	const processGroup = (group: Group) => {
+		groups.push(group);
+	}
+
+	const processRow = (row: Row) => {
+		for (const g of row.components) processGroup(g);
+	}
+
+	const processSection = (section: Section) => {
+		for (const g of section.groups) {
+			if (g.type == 'group') processGroup(g as Group);
+			else if (g.type == 'row') processRow(g as Row);
+		}
+	}
+
+	for (const section of schema) processSection(section);
+
+	return groups;
+}
+
+export function serialize(data: { [key: string]: number | string }, schema: Section[]) {
+	let out = 0n;
+	const processGroup = (group: Group) => {
+		assert(group.component.type in constructorMap);
+		const constructor = constructorMap[group.component.type];
+		out = constructor.serialize(data[group.component.valueID] as never, out, group.component as never);
+	}
+
+	extractGroups(schema).forEach(processGroup);
+	return out;
+}
+
+export function deserialize(data: bigint, schema: Section[]) {
+	const groups = extractGroups(schema);
+	groups.reverse();
+	const out: { [key: string]: number | string } = {};
+	
+	const processGroup = (group: Group) => {
+		assert(group.component.type in constructorMap);
+		const constructor = constructorMap[group.component.type];
+		const res = constructor.deserialize(data, group.component as never);
+		out[group.component.valueID] = res.data;
+		data = res.remaining;
+	}
+
+	groups.forEach(processGroup);
+
+	return out;
 }
 
 export default class FormClass {
