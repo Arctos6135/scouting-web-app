78866a3f388650a516e7aef38cc3d5ef2ab2b484
Alex Waese-Perlman <alexwaeseperlman@gmail.com>
2022-03-21 14:17:36 -0400

    add unit tests

---
+++ shared/dataClasses/FormClass/index.test.ts
@@ -0,0 +1,16 @@
+import { serialize, deserialize, Section } from '.';
+
+test('form serialize', () => {
+	const testSchema: Section[] = [ { "type": "section", "groups": [ { "type": "row", "components": [ { "type": "group", "label": "", "component": { "min": 0, "max": 10, "increment": 0.01, "type": "num", "valueID": "a" } }, { "type": "group", "label": "", "component": { "min": 0, "increment": 1, "max": 10, "type": "num", "valueID": "b" } }, { "type": "group", "label": "", "component": { "type": "text", "valueID": "c", "charset": "xyz", "minlength": 0, "maxlength": 5 } } ] }, { "type": "group", "label": "", "component": { "options": [ "x", "y", "z" ], "type": "picker", "valueID": "d" } }, { "type": "group", "label": "", "component": { "type": "text", "valueID": "e", "charset": "xyz", "maxlength": 20 } } ] } ];
+	const data = {
+		'a': 3.05,
+		'b': 8,
+		'c': 'xxxy',
+		'd': 'y',
+		'e': 'yyxxz'
+	};
+	const serialized = deserialize(serialize(data, testSchema), testSchema);
+
+	for (let i in data) if (typeof data[i] == 'number') data[i] = (expect as any).closeTo(data[i]);
+	expect(serialized).toEqual(data);
+});
